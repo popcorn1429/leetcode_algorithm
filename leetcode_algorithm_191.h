54cea7c31bb59f35fd58ceb1eef8531c0eca7ecf
popcorn1429 <daileihit@126.com>
2019-01-25 10:46:03 +0800

    add solutions 190 & 191

---
+++ leetcode_algorithm_191.h
@@ -0,0 +1,16 @@
+#ifndef __leetcode_algorithm_191_h__
+#define __leetcode_algorithm_191_h__
+#include "basic_headers.h"
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int bits = sizeof(uint32_t) * 8;
+        int weight = 0;
+        for (int i = 0; i < bits; ++i) {
+            if ((n & (1 << i)) != 0)
+                ++weight;
+        }
+        return weight;
+    }
+};
+#endif /*__leetcode_algorithm_191_h__*/
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "leetcode_algorithm_191.h"
+
+int main() {
+    Solution s;
+    /**********start code***********/
+    uint32_t n = 11;
+    uint32_t r = s.hammingWeight(n);
+    cout << "hamming weight of " << n << " is " << r << endl;
+    /***********end code***********/
+    system("pause");
+    return 0;
+}
