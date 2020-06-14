5e03f4ab2a481cf63a2a03d6f4e1ccfd2d5ed175
popcorn1429 <30331532+popcorn1429@users.noreply.github.com>
2020-06-14 14:04:28 +0800

    Update and rename leetcode_algorithm_32.h to leetcode_algorithm_00032.h

--- leetcode_algorithm_00032.h
+++ leetcode_algorithm_00032.h
@@ -1,5 +1,5 @@
-#ifndef __leetcode_algorithm_32_h__
-#define __leetcode_algorithm_32_h__
+#ifndef __leetcode_algorithm_00032_h__
+#define __leetcode_algorithm_00032_h__
 #include <string>
 using namespace std;
 
