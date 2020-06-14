5ea04c3a3720bc54ab6217dfe24e3fd52ead32ef
popcorn1429 <30331532+popcorn1429@users.noreply.github.com>
2020-06-14 13:28:40 +0800

    Update and rename leetcode_algorithm_16.h to leetcode_algorithm_00016.h

--- leetcode_algorithm_00016.h
+++ leetcode_algorithm_00016.h
@@ -1,5 +1,5 @@
-#ifndef __leetcode_algorithm_16_h__
-#define __leetcode_algorithm_16_h__
+#ifndef __leetcode_algorithm_00016_h__
+#define __leetcode_algorithm_00016_h__
 #include <vector>
 #include <algorithm>
 using namespace std;
