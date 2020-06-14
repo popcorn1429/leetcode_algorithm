ed6e20be461f58809a05e03079b610810cf96379
popcorn1429 <30331532+popcorn1429@users.noreply.github.com>
2020-06-14 15:08:12 +0800

    Update and rename leetcode_algorithm_51_52.h to leetcode_algorithm_00051_00052.h

--- leetcode_algorithm_00051_00052.h
+++ leetcode_algorithm_00051_00052.h
@@ -1,13 +1,13 @@
 /*
 N-Queens
-    The n-queens puzzle is the problem of placing n queens on an n°¡n chessboard such that no two queens attack each other.
+    The n-queens puzzle is the problem of placing n queens on an n¬°√Ån chessboard such that no two queens attack each other.
     Given an integer n, return all distinct solutions to the n-queens puzzle.
 
     Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
 */
 
-#ifndef _leetcode_algorithm_51_52_h_
-#define _leetcode_algorithm_51_52_h_
+#ifndef _leetcode_algorithm_00051_52_h_
+#define _leetcode_algorithm_00051_52_h_
 #include "basic_headers.h"
 class Solution {
 public:
