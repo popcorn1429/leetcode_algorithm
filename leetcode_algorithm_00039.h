f4da2cc89f39ff03f19c130ce25fb65069d17cf3
popcorn1429 <30331532+popcorn1429@users.noreply.github.com>
2020-06-14 15:03:28 +0800

    Update and rename leetcode_algorithm_39.h to leetcode_algorithm_00039.h

--- leetcode_algorithm_00039.h
+++ leetcode_algorithm_00039.h
@@ -1,5 +1,5 @@
-#ifndef __leetcode_algorithm_39_h__
-#define __leetcode_algorithm_39_h__
+#ifndef __leetcode_algorithm_00039_h__
+#define __leetcode_algorithm_00039_h__
 #include <vector>
 using namespace std;
 
